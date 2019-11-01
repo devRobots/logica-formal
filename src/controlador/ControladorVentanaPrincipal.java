0cd88c8b0e69b92a4de27f2bc42312058c95fdac
sammy <smi31rincon@gmail.com>
2019-11-01 12:13:24 -0500

    Interfaz

--- src/controlador/ControladorVentanaPrincipal.java
+++ src/controlador/ControladorVentanaPrincipal.java
@@ -1,9 +1,60 @@
 package controlador;
 
+import java.util.ArrayList;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
 public class ControladorVentanaPrincipal {
 
-	public ControladorVentanaPrincipal() {
-		// TODO Auto-generated constructor stub
-	}
+    @FXML
+    private TextArea textArea;
+    private ArrayList<String> formula=new ArrayList<String>();
 
+    @FXML
+    void agregarParentesisIzq(ActionEvent event) {
+    }
+
+    @FXML
+    void agregarParentesisDer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void agregarNegacion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void agregarConjuncion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void agregarDisyuncion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void agregarCondicionalIzq(ActionEvent event) {
+
+    }
+
+    @FXML
+    void agregarCondicionalDer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void agregarBicondicional(ActionEvent event) {
+
+    }
+
+    private void transformar() {
+    	for(char aux:textArea.getText().toCharArray()) {
+    		
+    	}
+    }
 }
+
